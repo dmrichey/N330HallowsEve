9c7515599b7841d123e13f45c6b2ba7b7c1e7d19
dmrichey <dmrichey@iu.edu>
2022-03-06 12:01:53 -0500

    Updated Character Movement with Character Controller

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -4,39 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float turnSpeed = 10.0f;
-    public float runSpeed = .2f;
-    public float walkSpeed = .1f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public float runSpeed = 2f;
+    public float walkSpeed = 1f;
     public float currentSpeed;
+    public Transform camera;
 
-    Animator    m_anim;
-    Rigidbody   m_rb;
-    Vector3     m_movement;
-    Quaternion  m_rotation = Quaternion.identity;
+    Animator                m_anim;
+    CharacterController     m_char;
+    Vector3                 m_movement;
+    Quaternion              m_rotation = Quaternion.identity;
+    bool                    m_isMoving;
 
     // Start is called before the first frame update
     void Start()
     {
         m_anim = GetComponent<Animator>();
-        m_rb = GetComponent<Rigidbody>();
+        m_char = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
         m_movement.Set(horizontal, 0.0f, vertical);
         m_movement.Normalize();
 
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0.0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0.0f);
-        bool isMoving = hasHorizontalInput || hasVerticalInput;
+        if (m_movement.magnitude >= 0.1f)
+        {
+            m_isMoving = true;
+        } else
+        {
+            m_isMoving = false;
+        }
 
         currentSpeed = runSpeed;
 
-        if (isMoving)
+        if (m_isMoving)
         {
             m_anim.SetInteger("AnimState", 1);
             if (Input.GetKey(KeyCode.LeftShift))
@@ -51,10 +58,15 @@ public class PlayerController : MonoBehaviour
             m_anim.SetInteger("AnimState", 0);
         }
 
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_movement, turnSpeed * Time.deltaTime, 0.0f);
-        m_rotation = Quaternion.LookRotation(desiredForward);
+        if (m_isMoving)
+        {
+            float targetAngle = Mathf.Atan2(m_movement.x, m_movement.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
 
-        this.transform.position += (m_movement * currentSpeed);
-        m_rb.MoveRotation(m_rotation);
+            Vector3 moveDir = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+            m_char.Move(moveDir.normalized * currentSpeed * Time.deltaTime);
+        }
+        
     }
 }
